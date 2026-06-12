ed656962bbe448cbe19b4ec761af033ffde966c4
Alex <alex.porter7@outlook.com>
2026-06-11 19:38:51 -0500

    worked on message binaries and created a test bin for it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use crate::menu::menu::show_main_menu;
-use crate::menu::terminal_ui::{MessageScreen, MessageViewScreen};
+use crate::menu::terminal_ui::MessageScreen;
+use crate::message::message::{Message, MessageBinary, MessageMetadata};
+use chrono::{Timelike, Utc};
 
 pub mod message;
 pub mod menu;
@@ -14,7 +15,41 @@ fn main() {
     // mvc.load();
 
     let mut mvc2 = MessageScreen::from_empty();
-    mvc2.load();
+    //mvc2.load();
+
+    let mut test_bin = MessageBinary::from_file("RealTest1.bin");
+
+    println!("{test_bin}");
+
+
+    // let mut test_bin =
+    //     MessageBinary::from_empty("Real Test 1".to_string(),
+    //                               "RealTest1.bin".to_string());
+
+    // for i in 0..117 {
+    //
+    //     let test_meta = MessageMetadata {
+    //         created: Utc::now().second(),
+    //         shifts: i % 7,
+    //         has_read: i > 55,
+    //         read_timestamp: None,
+    //     };
+    //
+    //     let mut _message_name = String::from("Message Number ");
+    //     _message_name.push_str(i.to_string().as_str());
+    //
+    //     let mut _content = String::from("Oh man here we go we're on message number: ");
+    //     _content.push_str(i.to_string().as_str());
+    //
+    //     let _message = Message::new(test_meta, "Alex".to_string(), _message_name, _content);
+    //
+    //     println!("[{i}] {}", _message);
+    //
+    //     test_bin.add_message(_message);
+    //
+    // }
+    //
+    // test_bin.write_bin();
 
 
     //mvc.load();
--- src/message.rs
+++ src/message.rs
@@ -58,6 +58,22 @@ pub mod message {
             }
         }
 
+        pub fn from_file(file_name: &str) -> Self {
+            let base_path = get_option(&"message_binary_path".to_string());
+
+            let mut file = File::open(base_path + "/" + file_name)
+                .expect("Unable to open message binary");
+
+            let mut data_buffer = String::new();
+            file.read_to_string(&mut data_buffer)
+                .expect("Unable to read message binary");
+
+            let (m, _): (MessageBinary, _) = oxicode::decode_from_hex(&data_buffer)
+                .expect("Error decoding bin contents");
+
+            m
+        }
+
         pub fn archive_binary(path: &str) {
             //close references to binary (if necessary) and move to archived folder
             println!("not implemented {path}");
@@ -76,6 +92,11 @@ pub mod message {
             file.write_all(data.as_bytes())
                 .expect("Error writing to file");
 
+        }
+
+        pub fn add_message(&mut self, mes: Message) {
+            self.mes_in_binary += 1;
+            self.messages.push(mes);
         }
 
     }
@@ -135,6 +156,13 @@ pub mod message {
             Author: {}\n\
             Content:\n\
             {}", self.name, "N/A", self.author, self.content)
+        }
+    }
+
+    impl std::fmt::Display for MessageBinary {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Message Binary Name: [{}] | Messages in Binary: [{}]",
+                   self.name, self.mes_in_binary)
         }
     }
 }
